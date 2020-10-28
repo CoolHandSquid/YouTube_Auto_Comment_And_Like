7af5295b4c4a12eb859095a4537be46075a217cf
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2020-10-27 23:04:29 -0400

    Add files via upload

---
+++ YouTube_Auto_Comment_And_Like.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import os
+import calendar
+from datetime import date
+import time
+
+channel_url	= "https://www.youtube.com/channel/UCsTKuPfW15Zu8lVa1gDqlNQ"
+#video_name = "Tuesday October 27, 2020"	#Commented out because the get_name variable covers this for my useage
+my_comment = "Another bias broadcast by NPR Up First. I am embarrassed that we are funding this."
+like = False #False will dislike the video. True will like the video
+
+def get_name():
+	global video_name, video_name2
+	my_date = date.today()
+	wday 	= str(calendar.day_name[my_date.weekday()])
+	year 	= str(my_date.year)
+	day 	= str(my_date.day)
+	month 	= str(calendar.month_name[my_date.month])
+	video_name 	= "{}, {} {}, {}".format(wday, month, day, year)
+	video_name2 = "{} {} {}, {}".format(wday, month, day, year)
+	return video_name, video_name2
+	
+def comment(video_name, video_name2, like):
+	options = Options()
+	#options.add_argument('--headless')
+	options.add_argument('--no-sandbox')
+	options.add_argument('--disable-dev-shm-usage')
+	options.add_argument('user-data-dir=/root/.config/google-chrome')
+	driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
+	driver.get(channel_url)
+	try:
+		link_button = driver.find_element_by_link_text(video_name)
+		link_button.click()
+	except:
+		link_button = driver.find_element_by_link_text(video_name2)
+		link_button.click()
+	time.sleep(1)
+	elem = driver.find_element_by_tag_name('html')
+	time.sleep(1)
+	for i in range(10):
+		elem.send_keys(Keys.DOWN)
+
+	time.sleep(1)
+	if like == True:
+		i = 1
+	else:
+		i = 2
+	link_button = driver.find_element_by_xpath('//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[{}]/a'.format(i))
+	link_button.click()
+	time.sleep(1)
+	link_button = driver.find_element_by_xpath('//*[@id="placeholder-area"]')
+	link_button.click()
+	#time.sleep(1)
+	comment = driver.find_element_by_xpath('//*[@id="contenteditable-root"]')
+	#time.sleep(1)
+	comment.send_keys(my_comment)
+	#time.sleep(1)
+	link_button = driver.find_element_by_xpath('//*[@id="submit-button"]/a')
+	#time.sleep(1)
+	link_button.click()
+
+def main():
+	get_name()
+	print("Commenting on and like-or-dislikng {} and {}".format(video_name, video_name2))
+	#print("Commenting on and like-or-dislikng {}".format(video_name))
+	comment(video_name, video_name2, like)
+	#input("YEE")
+main()
+#f12, ctrl+ shift+c, click where you want to locate, rightclick in the analyzer, copy > copy xpath ### to find xpath
+
+
---
+++ YouTube_Auto_Comment_And_Like_Config.py
@@ -0,0 +1,43 @@
+#!/usr/bin/pyhton3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import os
+
+def os_init():
+os.chdir("/opt")
+os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+os.system("apt install gdebi-core")
+os.system("gdebi google-chrome-stable_current_amd64.deb")
+os.system("python3 -m pip install selenium")
+os.system("wget https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_linux64.zip")
+os.system("unzip chromedriver_linux64.zip")
+os.system("ln -sf /opt/chromedriver /usr/bin/chromedriver")
+
+"""
+cd /opt
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install gdebi-core
+gdebi google-chrome-stable_current_amd64.deb
+python3 -m pip install selenium
+wget https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+ln -sf /opt/chromedriver /usr/bin/chromedriver
+"""
+
+def init_persistant_cookie():
+	options = Options()
+	#options.add_argument('--headless')
+	options.add_argument('--no-sandbox')
+	options.add_argument('--disable-dev-shm-usage')
+	options.add_argument('user-data-dir=/root/.config/google-chrome')
+	driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
+	driver.get(channel_url)
+	input('Be sure to put log in to have the persistant cookie saved\n')
+	os.system('killall chrome')
+
+def main():
+	os_init()
+	init_persistant_cookie()
+
+main()
